9e58d11b796e4a233671c46ca878efc71150e553
jaimecreer <70456956+jaimecreer@users.noreply.github.com>
2020-08-30 22:54:46 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.forever(function () {
-	
-})
+function drawRandomDots (goal: number) {
+    current = 0
+    while (current < goal) {
+        x = randint(0, 4)
+        y = randint(0, 4)
+        if (!(led.point(x, y))) {
+            led.plot(x, y)
+            current += 1
+        }
+    }
+}
+function drawDots () {
+    led.plot(2, 2)
+    led.plotBrightness(2, 3, 150)
+}
+function drawRandomDot () {
+    led.plot(randint(0, 4), randint(0, 4))
+}
+let y = 0
+let x = 0
+let current = 0
+drawRandomDots(15)
